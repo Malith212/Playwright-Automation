25d33e2fbf60b40876afaf343a19eea18344db81
Malith21 <111115039+Malith21@users.noreply.github.com>
2025-07-20 19:00:58 +0530

    first commit

---
+++ tests-examples/login.spec.js
@@ -0,0 +1,16 @@
+const { test } = require('@playwright/test');
+
+test ('Browser Context Playwright test', async ({browser})=>{
+
+    //chrome - plugins/ co okies
+    const context = await browser.newContext();
+    const page = await context.newPage();    
+    await page.goto('https://rahulshettyacademy.com/loginpagePractise/');
+
+});
+
+test.only('Page Playwright test', async ({page})=>{
+
+    await page.goto('https://google.com');
+
+});
