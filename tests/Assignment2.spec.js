388f5d8be28b2cb0388fd1a4cd619e12a03cc11b
Malith21 <111115039+Malith21@users.noreply.github.com>
2025-08-16 09:58:10 +0530

    Staged Chnages

--- tests/Assignment2.spec.js
+++ tests/Assignment2.spec.js
@@ -1,4 +1,5 @@
 const {test, expect} = require('@playwright/test'); 
+const { time } = require('console');
 
 test.only("Assignment 2", async ({page})=>{
 
@@ -20,6 +21,7 @@ test.only("Assignment 2", async ({page})=>{
     const productCart = page.locator(".card-body");
     const productName = "ADIDAS ORIGINAL";
 
+    //wait for one elemen appear in the dom for the selector match in the page 
     await page.waitForSelector(".card-body");
 
     const count=await productCart.count();
@@ -34,8 +36,59 @@ test.only("Assignment 2", async ({page})=>{
 
     }
 
-    //wait for 5 seconds
-    await page.waitForTimeout(5000);    
+    //assertion in add to cart page
+
+    await page.locator("[routerlink*='cart']").click();
+    await page.waitForSelector("h3:has-text('ADIDAS ORIGINAL')");
+    const bool = await page.locator("h3:has-text('ADIDAS ORIGINAL')").isVisible();
+    console.log(bool);
+    await expect(bool).toBeTruthy();
+
+
+    //checkout page
+
+    await page.locator("text=checkout").click();
+
+    //wait for 4 seconds
+    await page.waitForTimeout(4000);
+
+    const cardNumber="1212121212121212";
+    const expDate="05";
+    const expMonth="15";
+    const cvv="166";
+    const coupon="test";
+    const bankName="HDFC";
+
+    await page.locator('input.txt').nth(0).fill(cardNumber);
+    await page.locator('input.txt').nth(1).fill(cvv);
+    await page.locator('input.txt').nth(2).fill(bankName);
+    await page.locator("[name='coupon']").fill(coupon);
+
+    const dropdown = await page.locator(".ddl");
+    await dropdown.nth(0).selectOption(expDate);   
+    await dropdown.nth(1).selectOption(expMonth);
+
+    const type="ind";
+    await page.locator("[placeholder*='Select Country']").pressSequentially(type);
+
+    //Iteration to select country
+
+    const dropdownCounry=await page.locator(".ta-results");
+    await page.waitForSelector(".ta-results");
+
+    const countCounry=await dropdownCounry.locator("button").count();
+    console.log(countCounry);
+
+    const country=" India";
+    for(let i=0;i<countCounry;i++){
+        if(country===await dropdownCounry.locator("button").nth(i).textContent()){
+            await dropdownCounry.locator("button").nth(i).click();
+            break;
+        }
+    }
+
+
+    await page.pause();
 
     
 
