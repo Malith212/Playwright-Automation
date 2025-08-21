bb4e8631dd185a7f11ce34e2392a25e1ad7c0cf0
Malith21 <111115039+Malith21@users.noreply.github.com>
2025-08-21 12:28:56 +0530

    calnder validation added

--- tests/Assignment4_Calander.spec.js
+++ tests/Assignment4_Calander.spec.js
@@ -8,6 +8,8 @@ test.only("Assignment4_Calander", async ({page})=>{
     const day = "30";
     const year = "2023";
 
+    const ExpectedList = [month, day, year];
+
     await page.locator(".react-date-picker.react-date-picker--closed").click();
     await page.locator(".react-calendar__navigation__label").click();
     await page.locator(".react-calendar__navigation__label").click();
@@ -16,7 +18,16 @@ test.only("Assignment4_Calander", async ({page})=>{
     await page.locator(".react-calendar__year-view__months__month").nth(Number(month) - 1).click();
     await page.locator("//abbr[text()='"+day+"']").click();
 
-    await page.pause();
+    //calander validation
 
+    const inputs=await page.locator(".react-date-picker__wrapper input");
+    console.log(inputs);
+
+    for (let i=0;i<inputs.length;i++)
+    {
+        const value=inputs[i].getAttribute("value");
+        console.log(value);
+        expect(value).toEqual(ExpectedList[i]);
+    }
 
 })
