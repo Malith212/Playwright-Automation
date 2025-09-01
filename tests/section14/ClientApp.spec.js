899c403a5979f4052c04d46ed4b298ce134353d7
Malith21 <111115039+Malith21@users.noreply.github.com>
2025-09-01 21:31:31 +0530

    Dashboad part added

--- tests/section14/ClientApp.spec.js
+++ tests/section14/ClientApp.spec.js
@@ -1,11 +1,19 @@
 const { test, expect } = require('@playwright/test');  
 const { LoginPage } = require('./pageObject/loginPage');
+const { Dashboard } = require('./pageObject/dashBoard');
 
 test.only("Assignment 1 Login", async ({ page }) => {
-    const email = "navindumalith0@gmail";
+    const email = "navindumalith0@gmail.com";
     const password = "Mn20010810@#";
+    const ExpProduct = "ADIDAS ORIGINAL";
 
-    const loginPage = new LoginPage(page); // No conflict now
+    const loginPage = new LoginPage(page); 
     await loginPage.goTo();
     await loginPage.login(email, password);
+
+    const dashboard = new Dashboard(page);
+    await dashboard.addProductToCart(ExpProduct);
+    await dashboard.goToCheckout();
+
+
 });
---
+++ tests/section14/pageObject/dashBoard.js
@@ -0,0 +1,32 @@
+class Dashboard {
+  constructor(page) {
+    this.page = page;
+    this.products = page.locator(".card-body");
+    this.titles = this.products.locator("b");
+    this.checkout = page.locator("[routerlink*='cart']");
+  }
+
+  async addProductToCart(ExpProduct) {
+    const count = await this.products.count();
+    console.log(count);
+
+    const titles = await this.titles.allTextContents();
+    console.log(titles);
+
+    for (let i = 0; i < count; i++) {
+      if (titles[i] === ExpProduct) {
+        console.log("Product Found");
+        await this.products.nth(i).locator("text= Add To Cart").click();
+        break;
+      }
+    }
+  }
+
+  async goToCheckout() {
+    await this.checkout.click();
+    //add 3 seconds delay
+    await this.page.waitForTimeout(3000);
+  }
+}
+
+module.exports = { Dashboard };
