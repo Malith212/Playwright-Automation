84ea667912e9ec60f3d4ac5ee74eac78a482093c
Malith21 <111115039+Malith21@users.noreply.github.com>
2025-09-02 21:09:13 +0530

    custom configuration file created

--- tests/section14/ClientApp.spec.js
+++ tests/section14/ClientApp.spec.js
@@ -55,52 +55,52 @@ for (const data of dataSet) {
     await orderId.getOrderId();
   });
 
-  customTest.only(`Place Order ${data.email}`, async ({ page, testDataForOrder }) => {
-    const poManger = new POManger(page);
+  // customTest(`Place Order ${data.email}`, async ({ page, testDataForOrder }) => {
+  //   const poManger = new POManger(page);
 
-    //   const email = "navindumalith0@gmail.com";
-    //   const password = "Mn20010810@#";
-    //   const ExpProduct = "ADIDAS ORIGINAL";
+  //   //   const email = "navindumalith0@gmail.com";
+  //   //   const password = "Mn20010810@#";
+  //   //   const ExpProduct = "ADIDAS ORIGINAL";
 
-    const loginPage = poManger.getLoginPage();
-    await loginPage.goTo();
-    await loginPage.login(testDataForOrder.email, testDataForOrder.password);
+  //   const loginPage = poManger.getLoginPage();
+  //   await loginPage.goTo();
+  //   await loginPage.login(testDataForOrder.email, testDataForOrder.password);
 
-    //Product add to cart
-    const dashboard = poManger.getDashboard();
-    await dashboard.addProductToCart(testDataForOrder.ExpProduct);
-    await dashboard.goToCheckout();
+  //   //Product add to cart
+  //   const dashboard = poManger.getDashboard();
+  //   await dashboard.addProductToCart(testDataForOrder.ExpProduct);
+  //   await dashboard.goToCheckout();
 
-    //navigate to checkout
-    const checkout = poManger.getCheckout();
-    await checkout.gotochekout();
+  //   //navigate to checkout
+  //   const checkout = poManger.getCheckout();
+  //   await checkout.gotochekout();
 
-    //fill Card Details
+  //   //fill Card Details
 
-    //   const creditCardNumber = "4242 4242 4242 4242";
-    //   const cvv = "123";
-    //   const cardName = "sampath";
-    //   const coupenCode = "rahulshettyacademy";
-    //   const expDate = "31";
-    //   const expMonth = "11";
+  //   //   const creditCardNumber = "4242 4242 4242 4242";
+  //   //   const cvv = "123";
+  //   //   const cardName = "sampath";
+  //   //   const coupenCode = "rahulshettyacademy";
+  //   //   const expDate = "31";
+  //   //   const expMonth = "11";
 
-    const cardDetails = poManger.getCardDetails();
-    await cardDetails.fillCardDetails(
-      testDataForOrder.creditCardNumber,
-      testDataForOrder.cvv,
-      testDataForOrder.cardName,
-      testDataForOrder.couponCode,
-      testDataForOrder.expMonth,
-      testDataForOrder.expDate
-    );
+  //   const cardDetails = poManger.getCardDetails();
+  //   await cardDetails.fillCardDetails(
+  //     testDataForOrder.creditCardNumber,
+  //     testDataForOrder.cvv,
+  //     testDataForOrder.cardName,
+  //     testDataForOrder.couponCode,
+  //     testDataForOrder.expMonth,
+  //     testDataForOrder.expDate
+  //   );
 
-    //fill Shipping Information
-    const shippingInformation = poManger.getShippingInformation();
-    await shippingInformation.fillShippingInformation(" India");
-    await shippingInformation.clickCheckoutButton();
+  //   //fill Shipping Information
+  //   const shippingInformation = poManger.getShippingInformation();
+  //   await shippingInformation.fillShippingInformation(" India");
+  //   await shippingInformation.clickCheckoutButton();
 
-    //getOrderId and search dymancally whther it is in Orders Page
-    const orderId = poManger.getOrderId();
-    await orderId.getOrderId();
-  });
+  //   //getOrderId and search dymancally whther it is in Orders Page
+  //   const orderId = poManger.getOrderId();
+  //   await orderId.getOrderId();
+  // });
 }
