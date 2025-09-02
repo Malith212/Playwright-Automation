44989215bf77c99af9cbf331f4f0c22e90692109
Malith21 <111115039+Malith21@users.noreply.github.com>
2025-09-02 14:36:19 +0530

    external json file created

--- tests/section14/ClientApp.spec.js
+++ tests/section14/ClientApp.spec.js
@@ -1,20 +1,22 @@
 const { test, expect } = require("@playwright/test");
 const { POManger } = require("./pageObject/poManger");
+//Json-->string-->object
+const dataSet = require("../../utils/placeorder.json");
 
 test.only("Assignment 1 Login", async ({ page }) => {
   const poManger = new POManger(page);
 
-  const email = "navindumalith0@gmail.com";
-  const password = "Mn20010810@#";
-  const ExpProduct = "ADIDAS ORIGINAL";
+//   const email = "navindumalith0@gmail.com";
+//   const password = "Mn20010810@#";
+//   const ExpProduct = "ADIDAS ORIGINAL";
 
   const loginPage = poManger.getLoginPage();
   await loginPage.goTo();
-  await loginPage.login(email, password);
+  await loginPage.login(dataSet.email, dataSet.password);
 
   //Product add to cart
   const dashboard = poManger.getDashboard();
-  await dashboard.addProductToCart(ExpProduct);
+  await dashboard.addProductToCart(dataSet.ExpProduct);
   await dashboard.goToCheckout();
 
   //navigate to checkout
@@ -23,21 +25,21 @@ test.only("Assignment 1 Login", async ({ page }) => {
 
   //fill Card Details
 
-  const creditCardNumber = "4242 4242 4242 4242";
-  const cvv = "123";
-  const cardName = "sampath";
-  const coupenCode = "rahulshettyacademy";
-  const expDate = "31";
-  const expMonth = "11";
+//   const creditCardNumber = "4242 4242 4242 4242";
+//   const cvv = "123";
+//   const cardName = "sampath";
+//   const coupenCode = "rahulshettyacademy";
+//   const expDate = "31";
+//   const expMonth = "11";
 
   const cardDetails = poManger.getCardDetails();
   await cardDetails.fillCardDetails(
-    creditCardNumber,
-    cvv,
-    cardName,
-    coupenCode,
-    expMonth,
-    expDate
+    dataSet.creditCardNumber,
+    dataSet.cvv,
+    dataSet.cardName,
+    dataSet.couponCode,
+    dataSet.expMonth,
+    dataSet.expDate
   );
 
   //fill Shipping Information
